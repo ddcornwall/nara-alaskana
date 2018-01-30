037e2d20123b6e96f72ff1dc393fae9c78c020c6
Daniel Cornwall <32757825+ddcornwall@users.noreply.github.com>
2018-01-30 05:47:38 -0900

    Fixed display issues

--- search.js
+++ search.js
@@ -35,7 +35,7 @@ $recentLinks.text("Working on getting ten items from the NARA Alaska Digitizatio
 
 //Let user know if search fails
 var naraRequestTimeout = setTimeout(function(){
-$recentLinks.text("Search failed. Unable to get search results in reasonable time.");
+$("#recent").append("</br><p>Search or display timed out. I apologize for the inconvenience.</p>");
 }, 60000);
 
 
@@ -57,11 +57,10 @@ $("#recent").append("</br> Parent Series Title: " + response.opaResponse.results
 //$("#recent").append("</br> Creating Organization: " + response.opaResponse.results.result[i].description.fileUnit.parentSeries.creatingOrganizationArray.creatingOrganization.creator.termName);
 
 $("#recent").append("</br> There are " + response.opaResponse.results.result[i].objects.object.length + " digital objects associated with this record.");
-$("#recent").append("</br> First digital object found found at <a href = \"" + response.opaResponse.results.result[i].objects.object[0].file["@url"] + "\" target=\"_blank\">" + response.opaResponse.results.result[i].objects.object[i].file["@url"] + "</a> </br>" );
+$("#recent").append("</br> First digital object found found at <a href = \"" + response.opaResponse.results.result[i].objects.object[0].file["@url"] + "\" target=\"_blank\">" + response.opaResponse.results.result[i].objects.object[0].file["@url"] + "</a> </br>" );
 $("#recent").append("<img src = \"" + response.opaResponse.results.result[i].objects.object[0].thumbnail["@url"] + "\">");
 $("#recent").append("</br> Full record and additional objects available at <a href=\"https://catalog.archives.gov/id/" + response.opaResponse.results.result[i].naId + "\" target=\"_blank\"> https://catalog.archives.gov/id/" + response.opaResponse.results.result[i].naId + "</a>");
 $("#recent").append("</br> NaID = " + response.opaResponse.results.result[i].naId + "<p style=\"border-bottom-style: solid\"> ");
-
 } // end display loop
 clearTimeout(naraRequestTimeout);
 
